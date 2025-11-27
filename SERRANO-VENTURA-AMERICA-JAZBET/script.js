9b9f70a3938a0719dc5f3641e768467665540aaf
SERRANO-VENTURA-AMERICA-JAZBET-0455 <23328051370455@cetis155.edu.mx>
2025-11-27 14:59:45 -0600

    Update script.js

--- SERRANO-VENTURA-AMERICA-JAZBET/script.js
+++ SERRANO-VENTURA-AMERICA-JAZBET/script.js
@@ -1,39 +1,13 @@
-let cart = [];
-let total = 0;
-
-function addToCart(name, price) {
-  cart.push({ name, price });
-  total += price;
-
-  document.getElementById("cart-count").textContent = cart.length;
-  document.getElementById("cart-total").textContent = total;
-
-  updateCartItems();
-}
-
-function updateCartItems() {
-  const list = document.getElementById("items-list");
-  list.innerHTML = "";
-
-  cart.forEach((item, index) => {
-    const li = document.createElement("li");
-    li.textContent = `${item.name} - $${item.price}`;
-    
-    const removeBtn = document.createElement("button");
-    removeBtn.textContent = "Eliminar";
-    removeBtn.onclick = () => removeFromCart(index);
-
-    li.appendChild(removeBtn);
-    list.appendChild(li);
-  });
-}
-
-function removeFromCart(index) {
-  total -= cart[index].price;
-  cart.splice(index, 1);
-
-  document.getElementById("cart-count").textContent = cart.length;
-  document.getElementById("cart-total").textContent = total;
-
-  updateCartItems();
-}
+ // Catalogo de productos para la tienda
+window.catalogData = {
+  products: [
+    {
+      id: "p1",
+      name: "Audífonos Inalámbricos",
+      price: 799,
+      category: "electronica",
+      image: "https://sony.scene7.com/is/image/sonyglobalsolutions/wh-ch520_Primary_image?$categorypdpnav$&fmt=png-alpha"
+    },
+     
+  ]
+};
